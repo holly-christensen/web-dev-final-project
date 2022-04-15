3bbe31308ab19981f0d9ea1f418224ff4b12f7a0
Holly Christensen <holly.christensen@comcast.net>
2022-04-15 15:26:05 -0400

    update model index

--- app/models/index.js
+++ app/models/index.js
@@ -6,5 +6,11 @@ const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
 db.user = require("./user.model.js")(mongoose);
+db.episode = require("./episode.model.js")(mongoose);
+db.podcast = require("./podcast.model.js")(mongoose);
+db.admin = require("./admin.model.js")(mongoose);
+db.creator = require("./creator.model.js")(mongoose);
+db.review = require("./review.model.js")(mongoose);
+db.comment = require("./comment.model.js")(mongoose);
 
 module.exports = db;
