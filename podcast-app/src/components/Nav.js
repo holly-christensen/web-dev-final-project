0f6d60963d96305a4480883e130b587bc12faa1e
Holly Christensen <holly.christensen@comcast.net>
2022-04-24 16:00:15 -0400

    add navbar

--- podcast-app/src/components/Nav.js
+++ podcast-app/src/components/Nav.js
@@ -3,27 +3,23 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 const Nav = () => {
-    return(
-        <div className="list-group">
-            <Link to="/home" className="list-group-item">
-                Home
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/home" className="navbar-brand ps-4">
+                Podcast App
             </Link>
-            <Link to="podcasts" className="list-group-item">
-                Search
-            </Link>
-            <Link to="profile" className="list-group-item">
-                Profile
-            </Link>
-            <Link to="signin" className="list-group-item">
-                SignIn
-            </Link>
-            <Link to="signout" className="list-group-item">
-                SignOut
-            </Link>
-            <Link to="signup" className="list-group-item">
-                SignUp
-            </Link>
-        </div>
+            <div className="navbar-nav">
+                <Link to="home" className="nav-item nav-link">
+                    Home
+                </Link>
+                <Link to="podcasts" className="nav-item nav-link">
+                    Search
+                </Link>
+                <Link to="profile" className="nav-item nav-link">
+                    Profile
+                </Link>
+            </div>
+        </nav>
     )
 }
 export default Nav;
