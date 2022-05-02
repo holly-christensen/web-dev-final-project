360784cca709863c78470f417d717e09f9978f23
Holly Christensen <holly.christensen@comcast.net>
2022-05-01 21:42:09 -0400

    homepage lists

--- podcast-app/src/components/Homepage.js
+++ podcast-app/src/components/Homepage.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {useProfile} from "../contexts/profile-context";
 import {getPodcastsByCategoryAndRating, getPodcastsById, getPodcastsBySearchTerm} from "../useRequest";
 import {Link} from "react-router-dom";
+import SecureContent from "./secure-content";
 
 const Homepage = () => {
 
@@ -33,10 +34,10 @@ const Homepage = () => {
     }
 
     const getTrendingPodcasts = async () => {
-        // const trendingPods = await getPodcastsByCategoryAndRating("Comedy", 4);
-        const trendingPods = await getPodcastsBySearchTerm("Comedy");
+        // const trendingPods = await getPodcastsByCategoryAndRating("Cats");
+        const trendingPods = await getPodcastsBySearchTerm("Cats");
         console.log(trendingPods);
-        setTrendingPodcasts(trendingPods);
+        setTrendingPodcasts(trendingPods.podcasts.data);
     }
 
     const truncateDescription = (description) => {
@@ -46,7 +47,8 @@ const Homepage = () => {
     return (
         <div>
             <h1>Homepage</h1>
-            <h3>Podcasts you follow</h3>
+            <SecureContent>
+            <h3 className={"mt-3"}>Podcasts you follow</h3>
             <div className={"ps-0 mt-4 card-group"}>
                 {
                     followedPodcasts.map((podcast) => {
@@ -56,7 +58,7 @@ const Homepage = () => {
                                 <div className={"card-body"}>
                                     <h6 className={"card-title"}>{podcast.title}</h6>
                                     <p className={"text-muted"}>{truncateDescription(podcast.description)}</p>
-                                    <Link to={`/podcast/details/${podcast.id}`}
+                                    <Link to={`/podcasts/details/${podcast.id}`}
                                           className={"link text-decoration-none p-0 m-0"}>See more</Link>
                                 </div>
                             </div>
@@ -64,17 +66,19 @@ const Homepage = () => {
                     })
                 }
             </div>
-            <h3>Tending Podcasts</h3>
+            </SecureContent>
+            <h3 className={"mt-4"}>Trending Podcasts</h3>
             <div className={"ps-0 mt-4 card-group"}>
                 {
-                    trendingPodcasts.map((podcast) => {
-                        return (<div key={podcast.podcastId}>
+                    trendingPodcasts.map((podcast, index) => {
+                        index += 1;
+                        return (<div key={index}>
                             <div className={"card m-2"} style={{width: '15rem'}}>
                                 <img className={"card-img-top"} src={podcast.imageUrl} alt={podcast.title}/>
                                 <div className={"card-body"}>
                                     <h6 className={"card-title"}>{podcast.title}</h6>
                                     <p className={"text-muted"}>{truncateDescription(podcast.description)}</p>
-                                    <Link to={`/podcast/details/${podcast.id}`}
+                                    <Link to={`/podcasts/details/${podcast.id}`}
                                           className={"link text-decoration-none p-0 m-0"}>See more</Link>
                                 </div>
                             </div>
--- podcast-app/src/useRequest.js
+++ podcast-app/src/useRequest.js
@@ -104,16 +104,15 @@ export async function getPodcastEpisodes(id, page) {
     `);
 }
 
-export async function getPodcastsByCategoryAndRating(category, rating) {
+export async function getPodcastsByCategoryAndRating(category) {
     return await graphQLClient.request(gql`
-        
 query {
-    podcasts(filters: {categories: "Comedy", rating: {minRating: 4}) {
+    podcasts(filters: {categories: "Comedy", rating: {minRating: 3}) {
         data {
-            id,
             title,
-            imageUrl
+            id,
             description,
+            imageUrl
         }
     }
 }
